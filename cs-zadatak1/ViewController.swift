f67529d6d919562919c35b698b941dc7c6e4920d
analjubek <ana.ljubek@gmail.com>
2022-08-08 10:53:42 +0200

    first commit

---
+++ cs-zadatak1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cs-zadatak1
+//
+//  Created by Ana Ljubek on 08.08.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
