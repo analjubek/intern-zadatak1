c770a167723558e14a179086b6c20bf9f3e58476
analjubek <ana.ljubek@gmail.com>
2022-08-09 10:33:47 +0200

    Flow for MainViewController added, SceneDelegate removed, project setup completed.

--- cs-zadatak1/AppDelegate.swift
+++ cs-zadatak1/AppDelegate.swift
@@ -10,15 +10,47 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    var initialViewController: UIViewController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        initialViewController = MainViewController(nibName: "MainViewController", bundle: nil)
+        
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+
+        window!.rootViewController = initialViewController
+        window!.makeKeyAndVisible()
+        
         return true
+        
+        /*let rootViewController = MainViewController()
+         let navigationController = UINavigationController(rootViewController: rootViewController)
+         self.window = UIWindow(frame: UIScreen.main.bounds)
+         self.window?.rootViewController = navigationController
+         self.window?.makeKeyAndVisible()
+
+         return true*/
+        
+        /*
+        // if self.window is nil then set a new UIWindow object to self.window.
+        self.window = self.window ?? UIWindow()
+       
+        // Set self.window's background color to red.
+        self.window!.backgroundColor = UIColor.red
+   
+        // Create a ViewController object and set it as self.window's root view controller.
+        self.window!.rootViewController = MainViewController()
+   
+        // Make the window be visible.
+        self.window!.makeKeyAndVisible()
+        
+        return true*/
     }
 
     // MARK: UISceneSession Lifecycle
-
+/*
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -30,7 +62,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+*/
 
 }
 
---
+++ cs-zadatak1/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  cs-zadatak1
+//
+//  Created by Ana Ljubek on 09.08.2022..
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
