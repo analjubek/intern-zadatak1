590923626191306909f3b7e8ad0810708d86e30b
analjubek <ana.ljubek@gmail.com>
2022-08-10 10:18:52 +0200

    Text Field input check implemented in code.

--- cs-zadatak1/MainViewController.swift
+++ cs-zadatak1/MainViewController.swift
@@ -22,16 +22,39 @@ class MainViewController: UIViewController {
         view.backgroundColor = .white
     }
     
-    @IBAction func txtInputHorizontalEC(_ sender: Any) {
-        print(txtHorizontalEdge.text!)
+    @IBAction func txtInputHorizontalEC(_ sender: UITextField) {
+        let horizontalInput = txtHorizontalEdge.text!
+        print(horizontalInput)
+        if(isNumber(horizontalInput) || horizontalInput.isEmpty){
+            print("broj ili prazno")
+        }
+        else{
+            print("nije broj")
+        }
     }
     
-    @IBAction func txtInputVerticalEC(_ sender: Any) {
-        print(txtVerticalEdge.text!)
+    @IBAction func txtInputVerticalEC(_ sender: UITextField) {
+        let verticalInput = txtVerticalEdge.text!
+        print(verticalInput)
+        if(isNumber(verticalInput) || verticalInput.isEmpty){
+            print("broj ili prazno")
+        }
+        else{
+            print("nije broj")
+        }
     }
     
     @IBAction func btnDrawTUI(_ sender: Any) {
         print("Evo me")
+    }
+    
+    func isNumber(_ text: String) -> Bool{
+        if let intVal = Int(text){
+            return true
+        }
+        else{
+            return false
+        }
     }
     
 }
