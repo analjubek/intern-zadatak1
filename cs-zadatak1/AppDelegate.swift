ed996a3e647571b86b9b2cdf489c753e77cdd501
analjubek <ana.ljubek@gmail.com>
2022-08-11 12:01:23 +0200

    Coordinator and its protocol created, coordinator connected with AppDelegate.

--- cs-zadatak1/AppDelegate.swift
+++ cs-zadatak1/AppDelegate.swift
@@ -11,6 +11,24 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private var appCoordinator: AppCoordinator?  // 1
+    
+    /*private lazy var appCoordinator: AppCoordinator = {
+        return AppCoordinator(in: self.window!)
+    }()*/
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+            let appCoordinator = AppCoordinator(window: window) // 2
+            
+            self.window = window
+            self.appCoordinator = appCoordinator
+            
+            appCoordinator.start()  // 3
+            return true
+    }
+    /*
     var initialViewController: UIViewController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -24,45 +42,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window!.makeKeyAndVisible()
         
         return true
-        
-        /*let rootViewController = MainViewController()
-         let navigationController = UINavigationController(rootViewController: rootViewController)
-         self.window = UIWindow(frame: UIScreen.main.bounds)
-         self.window?.rootViewController = navigationController
-         self.window?.makeKeyAndVisible()
-
-         return true*/
-        
-        /*
-        // if self.window is nil then set a new UIWindow object to self.window.
-        self.window = self.window ?? UIWindow()
-       
-        // Set self.window's background color to red.
-        self.window!.backgroundColor = UIColor.red
-   
-        // Create a ViewController object and set it as self.window's root view controller.
-        self.window!.rootViewController = MainViewController()
-   
-        // Make the window be visible.
-        self.window!.makeKeyAndVisible()
-        
-        return true*/
-    }
-
-    // MARK: UISceneSession Lifecycle
-/*
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-*/
-
+    }*/
 }
 
